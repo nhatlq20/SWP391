b8eecb9200084b6a5175de99a4549952a5f2684a
Avyn <anlt.ce190367@gmail.com>
2026-03-25 23:44:01 +0700

    fix: AnLT_Hash-password

--- PharmacyLife/src/main/java/utils/HashMD5.java
+++ PharmacyLife/src/main/java/utils/HashMD5.java
@@ -8,7 +8,7 @@ public class HashMD5 {
     public static String getMd5(String input) {
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] messageDigest = md.digest(input.getBytes());
+            byte[] messageDigest = md.digest(input.getBytes(java.nio.charset.StandardCharsets.UTF_8));
             StringBuilder sb = new StringBuilder();
             for (byte b : messageDigest) {
                 sb.append(String.format("%02x", b));
